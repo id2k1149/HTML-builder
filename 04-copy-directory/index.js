254b1e7034b3ab7e5fd8cfa79260f8b6aa43c043
id2k1149 <id2k1149@gmail.com>
2022-05-22 10:47:53 -0400

    feat: add task#04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = fs.promises;
 
+const source = 'files';
+const destination = 'files-copy';
+
+const srcPath = path.join(__dirname, source);
+const dstPath = path.join(__dirname, destination);
+
+copyDir(srcPath, dstPath);
+
+async function copyDir(source, destination) {
+  
+  await fsPromises
+    .rm(destination,
+      { recursive: true, force: true },
+      err => {
+        if (err) throw err;
+      }
+    );
+  
+  await fsPromises
+    .mkdir(destination, 
+      { recursive: true },
+      err => {
+        if (err) throw err;
+      }
+    );
+
+  await fsPromises
+    .readdir(source, {withFileTypes: true})
+    .then(files => {
+      files.forEach(file => {
+        const srcFilePath = path.join(source, file.name);
+        const dstFilePath = path.join(destination, file.name);
+        fs.stat(srcFilePath, (err, stats) => {
+          if (err) throw err;
+          if (stats.isDirectory()) {
+            copyDir(srcFilePath, dstFilePath);
+          } else {
+            fsPromises.copyFile(srcFilePath, dstFilePath);
+          }
+        });
+      });         
+    });
+}

611cf9baab6029bb1e21cc5847e41fffaef5317a
id2k1149 <id2k1149@gmail.com>
2022-05-22 10:30:48 -0400

    feat: add task#03

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = fs.promises;
+
+const dirPath = path.join(__dirname, 'secret-folder');
+
+fsPromises
+  .readdir(dirPath, {withFileTypes: true})
+  .then(files => {
+    files.forEach(file => {
+      const filePath = path.join(dirPath, file.name);
+      fs.stat(filePath, (err, stats) => {
+        if(err) throw err;
+        if (stats.isFile()) {
+          console.log(file.name.replace('.', ' - '), '-', ((stats.size)/1024).toFixed(3)+'kb');
+        }
+      });
+    });         
+  });
+  
